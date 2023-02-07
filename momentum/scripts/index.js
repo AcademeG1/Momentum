f7c630b5660973d29183c44f6b33cfae77a4760d
academeg1 <m-ignatovich@hotmail.com>
2023-02-07 17:55:12 +0300

    feat: added weather and update weather

--- momentum/scripts/index.js
+++ momentum/scripts/index.js
@@ -1,4 +1,4 @@
-function showTime() {
+function showTime() { // функция времени
     const date = new Date();
     document.querySelector('.time').textContent = date.toLocaleTimeString();
     setInterval(showTime, showDate, showGreeting, 1000);
@@ -6,7 +6,7 @@ function showTime() {
 
 showTime();
 
-function showDate(lang = 'ru-Ru') {
+function showDate(lang = 'ru-Ru') { // функция даты
     const date = new Date();
     const options = {month: 'long', day: 'numeric', weekday: 'long'};
     document.querySelector('.date').textContent = date.toLocaleDateString(lang, options);
@@ -14,40 +14,40 @@ function showDate(lang = 'ru-Ru') {
 
 showDate('en-En');
 
-function getTimeOfDay() {
+function getTimeOfDay() { // функция врмени дня
     const date = new Date();
     const timeCycle = ['morning', 'afternoon', 'evening', 'night'];
     return timeCycle[Math.floor(date.getHours()/6)-1];
 }
 
-function showGreeting() {
+function showGreeting() { // вывод пожелания с функцией времени дня
     const timeOfDay = getTimeOfDay();
     document.querySelector('.greeting').textContent = `Good ${timeOfDay}`;
 }
 
 showGreeting();
 
-function setLocalStorage() {
+function setLocalStorage() { // запись имени в локал
     const name = document.querySelector('.name');
     localStorage.setItem('name', name.value);
 }
-window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('beforeunload', setLocalStorage); // перед обновление
 
 
-function getLocalStorage() {
+function getLocalStorage() { // получение имени из локал
     const name = document.querySelector('.name');
     if(localStorage.getItem('name')) {
         name.value = localStorage.getItem('name');
     }
 }
-window.addEventListener('load', getLocalStorage);
+window.addEventListener('load', getLocalStorage); // перед загрузкой
 
-function getRandomNum() {
+function getRandomNum() { // функция рандома
     const num = String(Math.floor(Math.random() * (21 - 1) + 1));
     return num.padStart(2, '0');
 }
 
-async function setBg(number) {
+async function setBg(number) { // установка фона
     const timeOfDay = getTimeOfDay();
     let bgNum = number;
     // document.body.style.background = `url(https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg)`
@@ -58,13 +58,13 @@ async function setBg(number) {
     };
 }
 
-let randomNum = getRandomNum();
-setBg(randomNum);
+let randomNum = getRandomNum(); // получения числа от 1 до 20
+setBg(randomNum); // и устновка фона
 
-document.querySelector('.slide-next').addEventListener('click', getSlideNext);
-document.querySelector('.slide-prev').addEventListener('click', getSlidePrev);
+document.querySelector('.slide-next').addEventListener('click', getSlideNext); // кнопка вперед
+document.querySelector('.slide-prev').addEventListener('click', getSlidePrev); // кнопка назад
 
-function getSlideNext() {
+function getSlideNext() { // кнопка вперед, обработка
     if (randomNum === '20' || +randomNum + 1 === 21) {
         randomNum = '01';
     } else {
@@ -74,7 +74,7 @@ function getSlideNext() {
     setBg(randomNum);
 }
 
-function getSlidePrev() {
+function getSlidePrev() { // кнопка назад обработка
     if (randomNum === '0' || +randomNum - 1 === 0) {
         randomNum = '20';
     } else {
@@ -83,3 +83,26 @@ function getSlidePrev() {
     }
     setBg(randomNum);
 }
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+city.addEventListener('change', () => {
+    getWeather(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`);
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+})
+
+async function getWeather(url) {
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&lang=ru&appid=a4d4f1ee98bf610a89f036359e940935&units=metric`;
+    const res = await fetch(url)
+    const data = await res.json();
+    console.log(data.weather[0].id, data.weather[0].description, data.main.temp)
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${data.main.temp}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+}
+
+getWeather('https://api.openweathermap.org/data/2.5/weather?q=%D0%9C%D0%B8%D0%BD%D1%81%D0%BA&lang=ru&appid=a4d4f1ee98bf610a89f036359e940935&units=metric');
